a056e5b5f87ca2ffa1cd7ca48ff84607a65b4f97
VictorUlian <yctor.ulian@gmail.com>
2022-05-03 10:12:21 -0300

    Initial files of the backend

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(5000, ()=> {
+    console.log("Back-end funcionando, nao esquece de desligar a cada atualizaçao")
+});
